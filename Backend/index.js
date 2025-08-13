7c6b33700d0376ad54156c45a46d5d88db79d729
Tushar Choudhari <tusharchoudhari0808@gmail.com>
2025-08-13 19:38:27 +0530

    Fifth commit: added routing for user list and create page

--- Backend/index.js
+++ Backend/index.js
@@ -1,30 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const userDB = require('./DB/db.js');
-const userRoute = require('./Route/user.js');
-const app = express()
-const port = 3000
+const userRoute = require('./Routes/RouteUsers.js');
+
+const app = express();
+const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+// Connect to PostgreSQL
 userDB.connect((err) => {
   if (err) {
     console.error('Error connecting to the database', err);
   } else {
     console.log('Connected to the database');
   }
-})
+});
 
-app.use('/user', userRoute);
+// Routes
+app.use('/api/users', userRoute);
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
+  console.log(`Server running on port ${port}`);
+});
